748f9c448339c01d15ba673778f45b1bc03bc934
Loba007 <78315976+Loba007@users.noreply.github.com>
2025-11-09 23:12:28 +0100

    Implement range checking in RangeChecker
    Fix syntax errors and add range checking logic.

---
+++ RangeChecker.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class RangeChecker{
+public static void main(String[] args){
+Scanner input = new (system.in); 
+
+System.out.print("Enter range of number between: ");
+int range = input.nextInt();
+
+if (range > 1 &&range <= 100){
+
+}else {
+System.out.println("Out of Range.");
+
+  }
+ }
+}
