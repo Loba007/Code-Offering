0550066fc5ccc33dae975ce7193cdf2fe7b57360
Loba007 <paulacious4greatness@gmail.com>
2025-11-09 16:40:36 +0100

    my first java code

---
+++ VotingSystem.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class VotingSystem {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter your age: ");
+int age = input.nextInt();
+if (age >= 18) {
+System.out.print("You are eligible to vote.");
+} else{
+System.out.println("You are not eligible to vote.");
+}
+
+}
+}
